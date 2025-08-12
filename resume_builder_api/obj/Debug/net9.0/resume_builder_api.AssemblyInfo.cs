f09ae0d42db5c720b80c141854f171f14296591b
Naresh Koirala <koiralanaresh10@gmail.com>
2025-08-11 21:10:55 -0600

    Remove unused files and clean up project structure for better maintainability

--- resume_builder_api/obj/Debug/net9.0/resume_builder_api.AssemblyInfo.cs
+++ resume_builder_api/obj/Debug/net9.0/resume_builder_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resume_builder_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d436d3aecd4c7b7e3e7c4154783a5b5ec009537d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb04a2ad9024af933ee31977e8466f3ceae3578c")]
 [assembly: System.Reflection.AssemblyProductAttribute("resume_builder_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resume_builder_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
